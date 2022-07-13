5454e3b88f2e7c9dfe42bbd2aa14b5e3e7199d1d
MerzCheung <1318038346@qq.com>
2022-07-13 17:11:00 +0800

    1

---
+++ java_guide/src/main/java/com/ming/zhang/java_guide/JavaGuideApplication.java
@@ -0,0 +1,13 @@
+package com.ming.zhang.java_guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGuideApplication.class, args);
+    }
+
+}
